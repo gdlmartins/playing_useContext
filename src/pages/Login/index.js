0c8a07a968ef573d88e5a6aa97f2ccf4599860d1
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2023-01-28 22:07:34 +0800

    first commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,64 @@
+import { Button } from '@material-ui/core';
+import {
+  Container,
+  Titulo,
+  InputContainer
+} from './styles';
+import {
+  Input,
+  InputLabel,
+  InputAdornment 
+} from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/context/Usuario';
+
+function Login() {
+  const history = useHistory();
+  return (
+    <Container>
+      <UsuarioContext.Consumer>
+        {({ nome, setNome, saldo, setSaldo }) => (
+          <>
+            <Titulo>
+              Insira o seu nome
+            </Titulo>
+            <InputContainer>
+              <InputLabel>
+                Nome
+              </InputLabel>
+              <Input
+                value={nome}
+                onChange={(evento) => setNome(evento.target.value)}
+                type="text"
+              />
+            </InputContainer>
+            <InputContainer>
+              <InputLabel>
+                Saldo
+              </InputLabel>
+              <Input
+              type="number"
+              value={saldo}
+              onChange={(evento) => setSaldo(evento.target.value)}
+              startAdornment={
+                <InputAdornment position="start">
+                  R$
+                </InputAdornment>
+              }
+            />
+            </InputContainer>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => history.push("/feira")}
+            >
+              Avançar
+            </Button>
+          </>
+        )}
+      </UsuarioContext.Consumer>
+    </Container>
+  )
+};
+
+export default Login;
